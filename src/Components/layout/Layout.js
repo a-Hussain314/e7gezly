7fcb065722eae673a57aa172436bbe01faf01e5a
ahmed hussain <ah0240089@gmail.com>
2021-02-05 02:55:17 +0200

    seo

--- src/Components/layout/Layout.js
+++ src/Components/layout/Layout.js
@@ -10,11 +10,7 @@ import Footer from "./Footer";
 function Layout({
     children,
     title = 'E7gezly',
-    description = `E7gezly is new-technology that helps patients
-     to queue their visit to X hospital without having to wait
-     inside the hospital for a long time due to covid-19 pandemic
-     all that through e7gezly website.`,
-
+    description="E7gezly is new-technology that helps patients to queue their visit to X hospital without having to wait inside the hospital for a long time due to covid-19 pandemic all that through e7gezly website."
   })  {
     return (<>
         <Helmet>
