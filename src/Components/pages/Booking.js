6dfae1ef32c217f34ef3f987be3add744e1ced82
ahmed hussain <ah0240089@gmail.com>
2021-02-22 02:02:30 +0200

    booking logic + api

--- src/Components/pages/Booking.js
+++ src/Components/pages/Booking.js
@@ -6,11 +6,14 @@ import "./Booking.scss";
 
 function Booking(props) {
     const[view, setView] = useState("book");
+    const[type, setType] = useState(null);
     const[spicializations, setSpicializations] = useState([]);
     const[spicializationsList, setSpicializationsList] = useState([]);
     const[doctors, setDoctors] = useState([]);
     const[doctorsList, setDoctorsList] = useState([]);
-    const[showCalender, setShowCalender] = useState(false);
+    const[bookingDetails, setBookingDetails]=useState(null);
+    const[showBookingButton, setShowBookingButton] = useState(false);
+    const[bookings, setBookings] = useState([])
 
     useEffect(()=>{
         if(!window.localStorage.getItem("userData")){
@@ -37,11 +40,23 @@ function Booking(props) {
                 console.log(error)
             })
 
+            fetchUserBookings();
+
         }
     },[])
 
 
-   
+   const weekDays = {
+       0 : "Sunday",
+       1 : "Monday",
+       2 : "Tuesday",
+       3 : "Wednesday",
+       4 : "Thursday",
+       5 : "Friday",
+       6 : "Saturday"
+   }
+
+   const AppoinmentPageRef = React.createRef();
 
    const typeSelectHandler = (e)=>{
     console.log(e.target.value);
@@ -49,10 +64,17 @@ function Booking(props) {
     if(e.target.value === "rediograpghy and tests"){
         // because the first spec.in list is "lab"
         setSpicializationsList([spicializations[0]])
+        setType("rediograpghy and tests")
     }
     else{
         // because the first spec.in list is "lab"
         setSpicializationsList(spicializations.slice(1))
+        if(e.target.value === "normal check up"){
+            setType("normal check up")
+        }
+        else if(e.target.value === "follow up"){
+            setType("follow up")
+        }
     }
    }
 
@@ -62,12 +84,38 @@ function Booking(props) {
    }
 
    const doctorSelectHandler = (e)=>{
-    console.log(e.target.value);
-    setShowCalender(true)
+    console.log(JSON.parse(e.target.value));
+    setBookingDetails(JSON.parse(e.target.value));
+    setShowBookingButton(true);
    }
 
    const formSumbitHandler = (e)=>{
     e.preventDefault();
+    console.log(bookingDetails);
+    Requester.post("/bookings/bookAppointment", bookingDetails)
+    .then((response)=>{
+        // console.log(response);
+        fetchUserBookings();
+        window.alert("Booked Successfully");
+        AppoinmentPageRef.current.click();
+    })
+    .catch((error)=>{
+        console.log(error);
+        window.alert("Booking Process Failed");
+    })
+   }
+
+
+   const fetchUserBookings = () => {
+    let booingsUrl = `/bookings/allBookingByUserId/${JSON.parse(window.localStorage.getItem("userData"))._id}`;
+    Requester.get(booingsUrl)
+    .then((response)=>{
+        // console.log(response)
+        setBookings(response.data)
+    })
+    .catch((error)=>{
+        console.log(error)
+    })
    }
 
   
@@ -79,7 +127,7 @@ function Booking(props) {
                         <div className="leftTable">
                             <ul>
                             <li onClick={()=>{setView("book")}}>Book Appointment</li>
-                            <li onClick={()=>{setView("list")}}>My Appointments</li>
+                            <li ref={AppoinmentPageRef} onClick={()=>{setView("list")}}>My Appointments</li>
                             </ul>
                         </div>
                         <div className="rightTable">
@@ -108,24 +156,34 @@ function Booking(props) {
                                 <label htmlFor="doctor" >select doctor</label>
                                 <select id="doctor" onChange={doctorSelectHandler}>
                                 <option  defaultValue value=""> -- select an option -- </option>
-                                    {doctorsList.map((doctor, index)=>{
+                                    {doctorsList.map(function(doctor, index){
                                         return(
-                                            <option key={index} value={doctor._id}>{doctor.name} - {doctor.specilizationName}</option>
+                                        <React.Fragment key={index}>
+                                            {doctor.availableDays.map((day, idx)=>{
+                                                    if(!!parseInt(day)){
+                                                        return(
+                                                            <option 
+                                                             key={`${index} - ${idx}`} 
+                                                             value={JSON.stringify({
+                                                                 doctorId:doctor._id,
+                                                                 userId: props.appState.userData._id,
+                                                                 bookingDay:index,
+                                                                 specializationId:doctor.specializationId,
+                                                                 bookingType: type,
+                                                                })}
+                                                            >
+                                                                {doctor.name} - {weekDays[idx]} - From {doctor.startTime}:00 To {doctor.endTime}:00 
+                                                            </option>
+                                                        )
+                                                    }
+                                                })
+                                            }
+                                            </React.Fragment>
                                         )
                                     })}
                                 </select>
                                 
-                                {showCalender && <>
-                                <input 
-                                  type="date" 
-                                  id="start" 
-                                  name="trip-start"
-                                  min="2021-02-17" 
-                                  max="2021-02-24"
-                                />
-                                <button className="btn1">Book Appointment</button>
-                                </>
-                                }
+                                {showBookingButton &&<button className="btn1">Book Appointment</button>}
 
                             </form>
                             }
@@ -133,36 +191,25 @@ function Booking(props) {
                             {view==="list" &&
                             <>
                              <h2>Appointments List</h2>
-                             <div className="appointment">
-                                <ul>
-                                    <li>{"name : Zell Liew"}</li>
-                                    <li>{"dr : john doe"}</li>
-                                    <li>{"specialization : Cardiologists"}</li>
-                                    <li>{"date and time  : Feb 17 2021 14:30"} </li>
-                                </ul>
-                                    <button className="btn2">Cancel</button>
-                             </div>
-                             <div className="appointment">
-                                <ul>
-                                    <li>{"name : Zell Liew"}</li>
-                                    <li>{"dr : john doe"}</li>
-                                    <li>{"specialization : Cardiologists"}</li>
-                                    <li>{"date and time  : Feb 17 2021 14:30"} </li>
-                                </ul>
-                                    <button className="btn2">Cancel</button>
-                             </div>
-                             <div className="appointment">
-                                <ul>
-                                    <li>{"name : Zell Liew"}</li>
-                                    <li>{"dr : john doe"}</li>
-                                    <li>{"specialization : Cardiologists"}</li>
-                                    <li>{"date and time : Feb 17 2021 14:30"} </li>
-                                </ul>
-                                    <button className="btn2">Cancel</button>
-                             </div>
-                               
-                             
-                            
+                             {bookings.map((booking, index)=>{
+                                 return(
+                                    <div key={index} className="appointment">
+                                        <ul style={{
+                                            color : booking.currentNumberInQeue > 0 ? "green" : "grey" 
+                                            }}>
+                                            <li>{`Name : ${booking.userData.firstName} ${booking.userData.lastName}`}</li>
+                                            <li>{`Dr : ${booking.doctorData.name}`}</li>
+                                            <li>{`Specialization : ${booking.doctorData.specilizationName}`}</li>
+                                            <li>{`Day and time  : ${weekDays[parseInt(booking.bookingDay)]} - from ${booking.doctorData.startTime}:00 To ${booking.doctorData.endTime}:00`} </li>
+                                            <li>{`Queue length : ${booking.totalNumberInQeue}`}</li>
+                                            <li>{`Your number in Queue : ${booking.numberInQeue}`}</li>
+                                            <li>{`Current in Queue: ${booking.currentNumberInQeue}`}</li>
+                                            <li>{`Queue status: ${booking.currentNumberInQeue > 0 ? "The Queue Started" : "The Queue Did not Started Yet"}`}</li>
+                                        </ul>
+                                            <button className="btn2">Cancel</button>
+                                    </div>
+                                 )
+                             })}
                             </>
                             }
                             
