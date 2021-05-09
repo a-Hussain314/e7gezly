5b20a19c55e41a1d8cc141b84eab8c1e2ed74dad
ahmed <hussain>
2021-05-09 23:09:40 +0200

    adding carousel and its sliders

--- src/Components/pages/Home.js
+++ src/Components/pages/Home.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Requester from "../../utilities/Requester";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 import "./Home.scss";
 
 
@@ -7,65 +9,115 @@ function Home(props) {
     const [news, setNews] = useState([]);
     const [advices, setAdvices] = useState([]);
 
+    const responsive = {
+        superLargeDesktop: {
+            // the naming can be any, depends on you.
+            breakpoint: { max: 4000, min: 3000 },
+            items: 1
+        },
+        desktop: {
+            breakpoint: { max: 3000, min: 1024 },
+            items: 1
+        },
+        tablet: {
+            breakpoint: { max: 1024, min: 464 },
+            items: 1
+        },
+        mobile: {
+            breakpoint: { max: 464, min: 0 },
+            items: 1
+        }
+    };
 
-    useEffect(()=>{
+    useEffect(() => {
         Requester.get("/home/allAdvices")
-        .then((response)=>{
-            // console.log(response)
-            setAdvices(response.data)
-        })
-        .catch((error)=>{
-            console.log(error)
-        })
+            .then((response) => {
+                // console.log(response)
+                setAdvices(response.data)
+            })
+            .catch((error) => {
+                console.log(error)
+            })
 
 
         Requester.get("/home/allNews")
-        .then((response)=>{
-            // console.log(response)
-            setNews(response.data)
-        })
-        .catch((error)=>{
-            console.log(error)
-        })
+            .then((response) => {
+                // console.log(response)
+                setNews(response.data)
+            })
+            .catch((error) => {
+                console.log(error)
+            })
 
 
-    },[])
+    }, [])
     return (
         <>
-                <section className="home_hero">
-                    <div className="container">   
+            <section className="home_hero">
+                <Carousel
+                    swipeable
+                    draggable
+                    showDots
+                    responsive={responsive}
+                    // means to render carousel on server-side.
+                    infinite={true}
+                    autoPlay
+                    autoPlaySpeed={500000}
+                    keyBoardControl={true}
+                    containerClass="carousel__container"
+                    renderDotsOutside
+                    arrows={true}
+                    // containerClass={styles.containerClass}
+                    // sliderClass={styles.sliderClass}
+                    // itemClass={styles.itemClass}
+                    // dotListClass={styles.dotListClass}
+                >
+                    <div className="container">
                         <div className="hero_container">
                             <h3>{"E7gezly is new-technology that helps patients to queue their visit to X hospital without having to wait inside the hospital for a long time due to covid-19 pandemic all that through e7gezly website."}</h3>
-                            <img src={require("../images/doctor.png")} alt={"doctor"}/>
-                        </div> 
+                            <img src={require("../images/doctor.png")} alt={"doctor"} />
+                        </div>
                     </div>
-                </section>
-                
-                {!!news.length && <section className="news">
-                    <div className="container">    
-                        <h3>Latest News</h3>
-                        {news.map((topic, index)=>{
-                            return(
+                    <div className="container">
+                        <div className="hero_container">
+                            <h3>{"E7gezly is new-technology that helps patients to queue their visit to X hospital without having to wait inside the hospital for a long time due to covid-19 pandemic all that through e7gezly website."}</h3>
+                            <img src={require("../images/doctor2.jpg")} alt={"doctor"} />
+                        </div>
+                    </div>
+                    <div className="container">
+                        <div className="hero_container">
+                            <h3>{"E7gezly is new-technology that helps patients to queue their visit to X hospital without having to wait inside the hospital for a long time due to covid-19 pandemic all that through e7gezly website."}</h3>
+                            <img src={require("../images/doctor3.jpg")} alt={"doctor"} />
+                        </div>
+                    </div>
+                </Carousel>
+            </section>
+
+            {!!news.length && <section className="news">
+                <div className="container">
+                    <h3>Latest News</h3>
+                    {news.map((topic, index) => {
+                        return (
                             <div className={"newBox"} key={index}>
                                 <h4>{topic.title}</h4>
-                                <p>{topic.content}</p>    
+                                <p>{topic.content}</p>
                             </div>)
-                        })}
-                    </div>
-                </section>}
+                    })}
+                </div>
+            </section>}
 
-                {!!advices.length && <section className="covid">
-                    <div className="container">    
-                        <h3>Covid-19 advices</h3>
-                        {advices.map((advice, index)=>{
-                            return(
+            {!!advices.length && <section className="covid">
+                <div className="container">
+                    <h3>Covid-19 advices</h3>
+                    {advices.map((advice, index) => {
+                        return (
                             <div key={index} className="tipBox">
                                 <h4>{advice.title}</h4>
-                                <p>{advice.content}</p>    
+                                <p>{advice.content}</p>
                             </div>)
-                        })}
-                    </div>
-                </section>}
+                    })}
+                </div>
+            </section>}
         </>
     )
 }
