7ad8ec1cf1bbd86ee2f4fdd8d8e4e37bbbab1629
ahmed <hussain>
2021-05-09 22:47:03 +0200

    special navbar tabs for "admin" user

--- src/Components/layout/Header.js
+++ src/Components/layout/Header.js
@@ -24,7 +24,7 @@ function Header(props)  {
                     <div className="userBox">
                         <img className="userAvatar" src={`https://dummyimage.com/100x100/fff/343996.png&text=+${props.appState.userData.firstName.trim()[0].toUpperCase()}+${props.appState.userData.lastName.trim()[0].toUpperCase()}+`} alt="username letters"/>
                         <Link to="/">{props.appState.userData.firstName} {props.appState.userData.lastName}</Link>
-                        <Link to="/Booking">Appoinments</Link>
+                        <Link to="/Booking">{props.appState.userData.type !== "admin" ? "Appoinments" : "dashboard"}</Link>
                         <span className="signOut" onClick={signOut}>Sign out</span>
                     </div>
                 :
